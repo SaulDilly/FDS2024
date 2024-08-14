33a6f579da753a5996f172ac5dabe664a6373ec4
SaulDilly <141246614+SaulDilly@users.noreply.github.com>
2024-08-14 21:55:26 +0000

    Criação carro SUV

--- Exercício1/src/App.java
+++ Exercício1/src/App.java
@@ -3,11 +3,11 @@ public class App {
     public static void exibeProgressoCarro(Carro c, TipoCombustivel tc, String s, int abastecimento) {
         System.out.println("\n\n----------------");
         System.out.println("\nAbastencendo carro "+ s +" com gasolina");
-        basico.abastece(tc, abastecimento);
+        c.abastece(tc, abastecimento);
         System.out.println(c);
         System.out.println("\nViajando com o carro "+s);
-        basico.viaja(250);
-        basico.viaja(150);
+        c.viaja(250);
+        c.viaja(150);
         System.out.println(c);
     }
 
@@ -16,6 +16,7 @@ public class App {
         Carro basico = new Carro("Basico", TipoCombustivel.GASOLINA, 10, 55);
         Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA, 6, 45);
         Carro utilitario = new Carro("Utilitario", TipoCombustivel.DIESEL, 5, 70);
+        Carro suv = new Carro("SUV", TipoCombustivel.FLEX, 8, 55);
 
         System.out.println("Tipos de veiculos:");
         System.out.println(basico);
@@ -25,5 +26,6 @@ public class App {
         exibeProgressoCarro(basico, TipoCombustivel.GASOLINA, "basico", 55).
         exibeProgressoCarro(esportivo, TipoCombustivel.GASOLINA, "esportivo", 45).
         exibeProgressoCarro(utilitario, TipoCombustivel.DIESEL, "utilitario", 70).
+        exibeProgressoCarro(suv, TipoCombustivel.GASOLINA, "suv", 55).
     }
 }
