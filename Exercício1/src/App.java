8817db60a2c8f0e29dce65c000b89c49c52014ae
SaulDilly <141246614+SaulDilly@users.noreply.github.com>
2024-08-14 21:47:53 +0000

    Criação carro esportivo

--- Exercício1/src/App.java
+++ Exercício1/src/App.java
@@ -1,18 +1,26 @@
 public class App {
+
+    public static void exibeProgressoCarro(Carro c, TipoCombustivel tc, String s, int abastecimento) {
+        System.out.println("\n\n----------------");
+        System.out.println("\nAbastencendo carro "+ s +" com gasolina");
+        basico.abastece(tc, abastecimento);
+        System.out.println(c);
+        System.out.println("\nViajando com o carro "+s);
+        basico.viaja(250);
+        basico.viaja(150);
+        System.out.println(c);
+    }
+
     public static void main(String[] args) throws Exception {
 
         Carro basico = new Carro("Basico", TipoCombustivel.GASOLINA, 10, 55);
+        Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA, 6, 45);
 
         System.out.println("Tipos de veiculos:");
         System.out.println(basico);
+        System.out.println(esportivo);
 
-        System.out.println("\n\n----------------");
-        System.out.println("\nAbastencendo carro basico com gasolina");
-        basico.abastece(TipoCombustivel.GASOLINA, 55);
-        System.out.println(basico);
-        System.out.println("\nViajando com o carro basico");
-        basico.viaja(250);
-        basico.viaja(150);
-        System.out.println(basico);
+        exibeProgressoCarro(basico, TipoCombustivel.GASOLINA, "basico", 55).
+        exibeProgressoCarro(esportivo, TipoCombustivel.GASOLINA, "esportivo", 45).
     }
 }
