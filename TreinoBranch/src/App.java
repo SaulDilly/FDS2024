6f01adeaa9c56331a091680d38c9e64d8d18bc86
SaulDilly <141246614+SaulDilly@users.noreply.github.com>
2024-08-19 21:18:53 +0000

    Alteração no branch main

--- TreinoBranch/src/App.java
+++ TreinoBranch/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         // Comentário inserido no branch main
-        System.out.println("Hello, World!");
+        System.out.println("Teste 1");
     }
 }
