b001b127ab8a2de4e0802b3dd77650429178df34
SaulDilly <141246614+SaulDilly@users.noreply.github.com>
2024-08-19 21:18:16 +0000

    Comentário inserido no branch main

---
+++ TreinoBranch/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Comentário inserido no branch main
+        System.out.println("Hello, World!");
+    }
+}
