23a39803e162aa942ce18741ff366d60d635b690
SaulDilly <141246614+SaulDilly@users.noreply.github.com>
2024-08-19 21:22:15 +0000

    PrimeiraAlteracao

--- TreinoBranch/src/App.java
+++ TreinoBranch/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        // Comentário inserido no branch main
+        // Comentário inserido no branch Alteracao
         System.out.println("Teste 1");
     }
 }
